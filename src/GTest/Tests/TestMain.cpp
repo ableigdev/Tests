131a5c08a2021c3a8f9408510703f0abd2f1093a
IgorYarovoy <igor.yarovoy.98@ukr.net>
2018-02-25 23:23:31 +0200

    I've wrote for some stl-functions

---
+++ src/GTest/Tests/TestMain.cpp
@@ -0,0 +1,114 @@
+#include <gtest\gtest.h>
+#include <vector>
+#include <list>
+#include <set>
+
+
+namespace
+{
+	bool isWholeNumbers(int value)
+	{
+		return value % 2 == 0;
+	}
+}
+
+TEST(IntervalFunction, VectorInsert)
+{
+	const int size = 5;
+	int data[size] = { 2, 5, 8, 9, 3 };
+	std::vector<int> expectResult = { 2, 5, 8, 9, 3 };
+
+	std::vector<int> vec;
+
+	vec.insert(vec.begin(), data, data + size);
+
+	EXPECT_EQ(vec.size(), expectResult.size());
+	EXPECT_EQ(vec, expectResult);
+}
+
+TEST(IntervalFunction, VectorErase)
+{
+	std::vector<int> v = { 3, 5, 7, 8, 1 };
+
+	v.erase(v.begin(), v.end());
+
+	EXPECT_TRUE(v.empty());
+}
+
+TEST(IntervalFunction, VectorAssign)
+{
+	std::vector<int> expectResult = { 4, 4, 4 };
+	std::vector<int> v;
+	v.assign(3, 4);
+
+	EXPECT_EQ(expectResult, v);
+	EXPECT_EQ(expectResult.size(), v.size());
+}
+
+TEST(RemoveErase, Vector)
+{
+	std::vector<int> v = { 3, 5, 7, 8, 1 };
+	std::vector<int> expectResult = { 3, 5, 8, 1 };
+
+	v.erase(std::remove(v.begin(), v.end(), 7), v.end());
+
+	EXPECT_EQ(expectResult, v);
+	EXPECT_EQ(expectResult.size(), v.size());
+}
+
+TEST(RemoveErase, List)
+{
+	std::list<int> v = { 3, 5, 7, 8, 1 };
+	std::list<int> expectResult = { 3, 5, 8, 1 };
+
+	v.remove(7);
+
+	EXPECT_EQ(expectResult, v);
+	EXPECT_EQ(expectResult.size(), v.size());
+}
+
+TEST(RemoveErase, Set)
+{
+	std::set<int> v = { 3, 5, 7, 8, 1 };
+	std::set<int> expectResult = { 3, 5, 8, 1 };
+
+	v.erase(7);
+
+	EXPECT_EQ(expectResult, v);
+	EXPECT_EQ(expectResult.size(), v.size());
+}
+
+TEST(RemoveIf, Vector)
+{
+	std::vector<int> v = { 3, 5, 7, 8, 1 };
+	std::vector<int> expectResult = { 3, 5, 7, 1 };
+
+	v.erase(std::remove_if(v.begin(), v.end(), isWholeNumbers), v.end());
+
+	EXPECT_EQ(expectResult, v);
+	EXPECT_EQ(expectResult.size(), v.size());
+}
+
+TEST(RemoveIf, List)
+{
+	std::list<int> v = { 3, 5, 7, 8, 1 };
+	std::list<int> expectResult = { 3, 5, 7, 1 };
+
+	v.remove_if(isWholeNumbers);
+
+	EXPECT_EQ(expectResult, v);
+	EXPECT_EQ(expectResult.size(), v.size());
+}
+
+TEST(RemoveIf, Set)
+{
+	std::set<int> v = { 3, 5, 7, 8, 1 };
+	std::set<int> expectResult = { 3, 5, 7, 1 };
+	std::set<int> temp;
+
+	std::remove_copy_if(v.begin(), v.end(), std::inserter(temp, temp.end()), isWholeNumbers);
+	v.swap(temp);	
+
+	EXPECT_EQ(expectResult, v);
+	EXPECT_EQ(expectResult.size(), v.size());
+}
