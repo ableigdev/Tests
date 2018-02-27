a8e7fecc609f26ffe07615c51946bd39440ad45f
IgorYarovoy <igor.yarovoy.98@ukr.net>
2018-02-28 01:37:51 +0200

    New tests and functions for tests

--- src/GTest/Tests/TestMain.cpp
+++ src/GTest/Tests/TestMain.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <list>
 #include <set>
+#include <cstdlib>
+#include <ctime>
 
 
 namespace
@@ -14,6 +16,41 @@ namespace
 	struct CompareString : public std::binary_function<std::string, std::string, bool>
 	{
 		// TODO: wriet operator()
+	};
+
+	struct TestStruct
+	{
+		int a;
+		std::string b;
+	};
+
+	std::vector<TestStruct> getFullVector()
+	{
+		std::srand(time(0));
+		TestStruct test;
+		std::vector<TestStruct> temp;
+		temp.reserve(5);
+
+		for (int i = 0; i < 5; ++i)
+		{
+			if (i % 2 == 0)
+				test.a = 4 + rand() % 3;
+			else
+				test.a = 5;
+			
+			test.b = "test";
+			temp.push_back(test);
+		}
+
+		return temp;
+	}
+
+	struct find_a : std::unary_function<TestStruct, bool>
+	{
+		bool operator()(const TestStruct& value)
+		{
+			return value.a == 5;
+		}
 	};
 }
 
@@ -130,5 +167,66 @@ TEST(SwapRemove, Vector)
 
 	EXPECT_EQ(v.size(), 3);
 	EXPECT_EQ(v.capacity(), 3);
+}
+
+TEST(InsertInMiddleVector, Test)
+{
+	std::vector<int> first = { 1, 3, 5, 9, 2, 4 };
+	std::vector<int> second = { 4, 8, 1 };
+	std::vector<int> expectResult = { 1, 3, 5, 4, 8, 1, 9, 2, 4 };
+
+	first.insert(first.begin() + first.size() / 2, second.begin(), second.end());
+
+	EXPECT_EQ(first, expectResult);
+	EXPECT_EQ(first.size(), expectResult.size());
+}
+
+TEST(InsertStrcutInVector, Test)
+{
+	std::vector<TestStruct> v = getFullVector();
+
+	std::vector<TestStruct>::iterator it = std::find_if(v.begin(), v.end(), find_a());
+	
+	EXPECT_EQ(it->a, 5);
+}
+
+TEST(MapInsert, Test)
+{
+	std::map<int, std::string> m;
+	std::map<int, std::string> expectResult = { { 4, "test" }, { 3, "test2" }, { 7, "test4"} };
+
+	m.insert(std::map<int, std::string>::value_type(4, "test"));
+	m.insert(std::map<int, std::string>::value_type(3, "test2"));
+	m.insert(std::map<int, std::string>::value_type(7, "test4"));
+
+	EXPECT_EQ(m, expectResult);
+	EXPECT_EQ(m.size(), expectResult.size());
+}
+
+TEST(MapUpdate, Test)
+{
+	std::map<int, std::string> m;
+	std::map<int, std::string> expectResult = { { 4, "test" }, { 3, "test6" }, { 7, "test111"} };
+
+	m.insert(std::map<int, std::string>::value_type(4, "test"));
+	m.insert(std::map<int, std::string>::value_type(3, "test2"));
+	m.insert(std::map<int, std::string>::value_type(7, "test4"));
+
+	m[3] = "test6";
+	m[7] = "test111";
+
+	EXPECT_EQ(m, expectResult);
+	EXPECT_EQ(m.size(), expectResult.size());
+}
+
+TEST(AdvanceDistance, Test)
+{
+	std::vector<int> v;
+	typedef std::vector<int>::iterator iter;
+	typedef std::vector<int>::const_iterator constIter;
+
+	constIter cIter;
+	iter i;
+	// TODO: coplete properies
 }
 
